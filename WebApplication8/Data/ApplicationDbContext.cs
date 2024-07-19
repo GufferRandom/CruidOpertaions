20992a9d5d919ed94316ddbb8407e24630a0e22c
GufferRandom <guffera2005@gmail.com>
2024-07-19 18:58:02 +0400

    Add project files.

---
+++ WebApplication8/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication8.Models;
+
+namespace WebApplication8.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        } 
+        DbSet<Category> Categories { get; set; }
+    }
+}
