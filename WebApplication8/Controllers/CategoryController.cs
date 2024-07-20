2ece0c83ed023f26598bbe9e79dc0a0b192087ff
GufferRandom <guffera2005@gmail.com>
2024-07-20 14:04:08 +0400

    Created add Category . not finished

---
+++ WebApplication8/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication8.Data;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+   
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete() { return View(); }
+
+        [HttpDelete]
+        public IActionResult Delete(string Name)
+        {
+            var obj = _db.Categories.Where(c => c.Name == Name).FirstOrDefault();
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WebApplication8/Models/Category.cs
+++ WebApplication8/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication8.Models
 {
@@ -7,7 +8,9 @@ namespace WebApplication8.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")] 
         public string Name { get; set; }
-        public string DisplayOrder{ get; set; }
+      
+        public int DisplayOrder{ get; set; }
     }
 }
